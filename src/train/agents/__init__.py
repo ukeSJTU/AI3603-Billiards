ac2c248595b75337f408519e22fc6b5bc12615f6
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-12-31 09:49:43 +0800

    feat(agent): initialize agent module with BasicAgent and RandomAgent imports

---
+++ src/train/agents/__init__.py
@@ -0,0 +1,5 @@
+from .base import ActionDict, Agent, BallsDict
+from .basic import BasicAgent
+from .random import RandomAgent
+
+__all__ = ["Agent", "BallsDict", "ActionDict", "BasicAgent", "RandomAgent"]
